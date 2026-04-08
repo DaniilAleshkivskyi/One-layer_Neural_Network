71fc86b82ce5bb7be323c0e6d5a25196480e259e
DaniilAleshkivskyi <milukovdaniil11@gmail.com>
2026-04-08 22:03:23 +0300

    developed getMatrix method,updated measureAccuracy method :>

--- src/Helpers/EvaluationMetrics.java
+++ src/Helpers/EvaluationMetrics.java
@@ -1,5 +1,6 @@
 package Helpers;
 
+import Models.ConfusionMatrix;
 import Models.MetricsModel;
 
 import java.util.List;
@@ -14,7 +15,22 @@ public class EvaluationMetrics {
                 correct++;
             }
         }
-        return new MetricsModel((double) correct / realClasses.size() * 100 ,String.format("%d/%d", correct, realClasses.size()));
+        ConfusionMatrix cM = ConfusionMatrix.getMatrix(realClasses, predictedClasses);
+        double p = calculatePrecision(cM);
+        double r = calculateRecall(cM);
+        double f = calculateF_Measure(p,r);
+        return new MetricsModel(p,r,f,String.format("===Measurements===\n%d/%d\nP = %d\nR = %d\nF = %d\n__________________", correct, realClasses.size(),p,r,f));
 
+    }
+
+    private static double calculatePrecision(ConfusionMatrix cM) {
+        return  (double) cM.pp() / (cM.pp() + cM.np());
+    }
+    private static double calculateRecall(ConfusionMatrix cM) {
+        return (double)  cM.pp() / (cM.pp() + cM.pn());
+    }
+
+    private static double calculateF_Measure(double p,double r) {
+        return (2 * p * r)/(p + r);
     }
 }
--- src/Models/ConfusionMatrix.java
+++ src/Models/ConfusionMatrix.java
@@ -1,3 +1,35 @@
 package Models;
 
-public record ConfusionMatrix(double pp,double pn,double np,double nn) {}
+import java.util.List;
+
+public record ConfusionMatrix(int pp, int pn, int np, int nn) {
+
+    public static ConfusionMatrix getMatrix(List<Integer> realClasses, List<Integer> predictedClasses) {
+        int pp = 0;
+        int pn = 0;
+        int np = 0;
+        int nn = 0;
+        var i = realClasses.iterator();
+        var j = predictedClasses.iterator();
+
+        while (i.hasNext()) {
+            int ansI = i.next();
+            int ansJ = j.next();
+            if (ansI == ansJ) {
+                if(ansI == 1) {
+                    pp++;
+                }else{
+                    nn++;
+                }
+            }else{
+                if(ansI == 1 & ansJ == 0) {
+                    pn++;
+                }else {
+                    pn++;
+                }
+            }
+        }
+
+        return new ConfusionMatrix(pp,pn,np,nn);
+    }
+}
